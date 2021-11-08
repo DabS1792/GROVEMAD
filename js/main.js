1b17ca28bb3e7c714d8630ad0a0dbe138c13726a
Игорь Дячук <dabsonwork2@gmail.com>
2021-11-08 18:06:32 +0200

    another commit

--- js/main.js
+++ js/main.js
@@ -8,7 +8,35 @@ $(function () {
         slidesToScroll: 4,
         autoplay: true,
         autoplaySpeed: 2500,
-        infinite:true,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1320,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+
+        ]
 
     });
 });
