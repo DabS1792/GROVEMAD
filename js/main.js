21d46d64ea32702784b79942e2f335607d153994
Игорь Дячук <dabsonwork2@gmail.com>
2021-11-08 16:11:33 +0200

    another commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.slider__box').slick({
+        arrows: false,
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        infinite:true,
+
+    });
+});
